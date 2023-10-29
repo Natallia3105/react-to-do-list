5c1d84f935e021d0220600e6caedc753745970e0
natalagoncaruk <honcharukna@gmail.com>
2023-10-29 14:23:54 +0200

    sadfas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import {Button} from "antd";
-
+import TodoList from "./TodoList";
+import { Typography } from 'antd';
+// TODO remove mock data
+const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+];
 function App() {
+
   return (
    <div>
-     <Button type="dashed">Кнопка</Button>
+       <Typography.Title>My todo list</Typography.Title>
+       <TodoList data={data}/>
+
    </div>
   );
 }
